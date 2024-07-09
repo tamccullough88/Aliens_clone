53445d4a13f0430792505ae559315fa2f9947ea6
Thomas McCullough <tamccullough88@gmail.com>
2024-07-08 22:25:41 -0400

    made a fleet of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -81,24 +81,30 @@ class AlienInvasion:
                 if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
 
-    def _create_alien(self, x_position):
+    def _create_alien(self, x_position, y_position):
             """Create an alien and place it in the row"""
             new_alien = Alien(self)
             new_alien.x = x_position
             new_alien.rect.x = x_position
+            new_alien.rect.y = y_position
             self.aliens.add(new_alien)
 
     def _create_fleet(self):
             """Create the fleet of aliens"""
             #Create an alien and keep adding aliens until there is no room left.
-            #Spacing between aliens is one alien width
+            #Spacing between aliens is one alien width and one alien height
             alien = Alien(self)
-            alien_width = alien.rect.width
-            current_x = alien_width
-            while current_x < (self.settings.screen_width - 2 * alien_width):
-                self._create_alien(current_x)
-                current_x += 2 * alien_width
+            alien_width, alien_height = alien.rect.size
 
+            current_x, current_y = alien_width, alien_height
+            while current_y < (self.settings.screen_height - 3 * alien_height):
+                while current_x < (self.settings.screen_width - 2 * alien_width):
+                    self._create_alien(current_x, current_y)
+                    current_x += 2 * alien_width
+
+                # Finished a row; reset x value, and increment y value
+                current_x = alien_width
+                current_y += 2 * alien_height
 
     def _update_screen(self):
             """update images on the screen and flip to the new screen."""
