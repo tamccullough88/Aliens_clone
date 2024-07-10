5e07ed62afe8a07439f08c137f0f6f04fc0d6e0a
Thomas McCullough <tamccullough88@gmail.com>
2024-07-09 21:48:58 -0400

    added functionality to make fleet move right, detect edge, drop down 1, then change directions, and repeat every time it hits an edge

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,6 +33,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
 
@@ -81,6 +82,11 @@ class AlienInvasion:
                 if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
 
+    def _update_aliens(self):
+            """Check if the fleet is at an edge, then update positions."""
+            self._check_fleet_edges()
+            self.aliens.update()
+
     def _create_alien(self, x_position, y_position):
             """Create an alien and place it in the row"""
             new_alien = Alien(self)
@@ -88,6 +94,19 @@ class AlienInvasion:
             new_alien.rect.x = x_position
             new_alien.rect.y = y_position
             self.aliens.add(new_alien)
+
+    def _check_fleet_edges(self):
+            """Respond appropriately if any aliens have reached an edge."""
+            for alien in self.aliens.sprites():
+                if alien.check_edges():
+                    self._change_fleet_direction()
+                    break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _create_fleet(self):
             """Create the fleet of aliens"""
